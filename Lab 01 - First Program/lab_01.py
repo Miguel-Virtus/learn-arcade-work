bbd8be33ac00332e3a314bd1c02a73f310be1113
Miguel-Virtus <78935896+Miguel-Virtus@users.noreply.github.com>
2021-02-24 22:33:23 +0100

    un chiste muy bueno

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,4 @@
-print("Hello World")
-print("mae mia todo el dia escribiendo lineas")
-print("vamos a imprimir muchas comillas \"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"")
+print("Aquí va un chiste para los curiosos: ")
+print(" Estos son dos señores, y le dice uno al otro")
+print(" - Oye, tu padre es un proscrito")
+print(" - Y el tuyo un tigretón")
